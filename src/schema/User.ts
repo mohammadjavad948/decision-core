713f4d74c64d866b83d0ceeec1f760829051769a
mohammad javad <mjkmo123@gmail.com>
2020-12-20 09:52:30 +0330

    finish user schema

--- src/schema/User.ts
+++ src/schema/User.ts
@@ -1,13 +1,20 @@
-import {Schema} from "mongoose";
+import {Schema, Document, Types} from "mongoose";
+import ObjectId = Types.ObjectId;
 
-interface UserI {
+interface UserI extends Document{
     name: string
     username: string
     password: string
-
+    classroom: ObjectId
 }
 
 
 const userSchema = new Schema({
-
+    name: String,
+    username: String,
+    password: String,
+    classroom: {
+        type: ObjectId,
+        ref: 'classroom'
+    }
 })
