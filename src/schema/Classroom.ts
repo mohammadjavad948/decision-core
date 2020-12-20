2df130108948691e940f82a4ed904d35e970854c
mohammad javad <mjkmo123@gmail.com>
2020-12-20 12:24:11 +0330

    create classroom schema

--- src/schema/Classroom.ts
+++ src/schema/Classroom.ts
@@ -1,5 +1,12 @@
-import {Document} from "mongoose";
+import {Document, model, Schema} from "mongoose";
 
 interface ClassroomI extends Document{
     name: string
 }
+
+const classroomSchema = new Schema({
+    name: String
+});
+
+
+export const Classroom = model<ClassroomI>('classroom', classroomSchema)
