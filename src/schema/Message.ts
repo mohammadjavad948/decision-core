464f8a243da71af8ffbcf9277ab21cbb57ec5c9c
mohammad javad <mjkmo123@gmail.com>
2020-12-21 18:37:33 +0330

    add typeId to Message schema

--- src/schema/Message.ts
+++ src/schema/Message.ts
@@ -5,6 +5,10 @@ import {MessageI} from "./interface/schemaInteface";
 const messageSchema = new Schema({
     text: String,
     type: String,
+    typeId: {
+      type: ObjectId,
+      ref: 'quiz'
+    },
     user: {
       type: ObjectId,
       ref: 'user'
