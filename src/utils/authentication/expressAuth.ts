4cd3534fa2939c51551dedc2882d518f16108cb7
mohammad javad <mjkmo123@gmail.com>
2020-12-21 09:14:22 +0330

    create login route

--- src/utils/authentication/expressAuth.ts
+++ src/utils/authentication/expressAuth.ts
@@ -1,3 +1,8 @@
-import {Router} from "express";
+import {Router, Request, Response} from "express";
 
 export const authRouter = Router();
+
+
+authRouter.post('/login', (req: Request, res: Response) => {
+
+});
