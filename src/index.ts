1358f744bcb8be07e1b79798f62896dde35b154f
mohammad javad <mjkmo123@gmail.com>
2020-12-21 09:19:47 +0330

    add body parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
+import * as bodyParser from "body-parser";
 import {DB, port} from "./variables";
 import {connect} from "mongoose";
 
 const app = express();
+
+// middleware
+app.use(bodyParser.json());
 
 app.listen(port || 3000, () => {
     console.log('server is running')
