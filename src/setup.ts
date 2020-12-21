5b5720b62f09d257eee77661cbce383a1904e0ba
mohammad javad <mjkmo123@gmail.com>
2020-12-21 21:43:09 +0330

    create setup file

---
+++ src/setup.ts
@@ -0,0 +1,48 @@
+import {connect} from "mongoose";
+import {DB} from "./variables";
+import {User} from "./schema/User";
+import {Channel} from "./schema/Channel";
+import {genSalt, hash} from "bcrypt";
+const prompt = require('prompt-sync')({sigint: true});
+
+async function run(){
+    try {
+
+        // connect to database
+        console.log('connecting to database ...');
+
+        await connect(DB, () => {
+            console.log('connected to db')
+        });
+
+        const name = prompt('What is your name?');
+        const username = prompt('Username?');
+        const rawPassword = prompt('password?');
+
+        // hash password
+        const salt = await genSalt(10);
+        const password = await hash(rawPassword, salt);
+
+        // save user
+        const user = new User({
+            name, username, password, admin: true, roles: []
+        });
+
+        await user.save();
+
+        console.log(user);
+
+        const channel = new Channel({
+           index: 0,
+           name: 'channel'
+        });
+
+        await channel.save()
+
+        console.log('you are done!');
+    }catch (e){
+        console.log('error happened! log: ' + e.toString())
+    }
+}
+
+run()
