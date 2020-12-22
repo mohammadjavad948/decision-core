ea876c21f972127f6836c5dca201faf7628fdf18
mohammad javad <mjkmo123@gmail.com>
2020-12-22 12:12:18 +0330

    create new channel function

--- src/socket/channel.ts
+++ src/socket/channel.ts
@@ -1,5 +1,26 @@
 import {Socket} from "socket.io";
+import {Channel} from "../schema/Channel";
+import {UserI} from "../schema/interface/schemaInteface";
 
 export function channelEmits(socket: Socket){
-    
+    // @ts-ignore
+    const user: UserI = socket.user;
+
+    // register events
+    socket.on('newChannel', newChannel)
+
+
+
+    // register functions
+    async function newChannel(name){
+
+        // check if user not admin
+        if (!user.admin) return null;
+
+        const count = await Channel.countDocuments({});
+        const channel = await new Channel({
+            name, index: count
+        });
+
+    }
 }
