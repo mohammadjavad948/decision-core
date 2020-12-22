354153a3dc916fcc11156a738155c7ae9c0ceb25
mohammad javad <mjkmo123@gmail.com>
2020-12-22 15:57:42 +0330

    create send message function

--- src/socket/message.ts
+++ src/socket/message.ts
@@ -1,6 +1,16 @@
 import {Socket} from "socket.io";
 
+interface UserMessage{
+    channelId: string
+    message: string
+    type: string
+}
+
 
 export function messageManager(io, socket: Socket){
+    socket.on('sendMessage', sendMessage)
 
+    async function sendMessage(data: UserMessage, callback){
+
+    }
 }
