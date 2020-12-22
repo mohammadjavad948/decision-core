b5a01261c2ab110ef94a72a9378213574f617037
mohammad javad <mjkmo123@gmail.com>
2020-12-22 08:20:26 +0330

    add get route for channel

--- src/utils/channel.ts
+++ src/utils/channel.ts
@@ -1,4 +1,16 @@
 import {Router} from "express";
+import {expressAuthMiddleware} from "../middleware/auth";
+import {Channel} from "../schema/Channel";
 
 
 export const channelRouter = Router();
+
+channelRouter.use(expressAuthMiddleware);
+
+channelRouter.get('/channels', async (req, res) => {
+
+    const channels = await Channel.find();
+
+    return res.send(channels);
+
+})
