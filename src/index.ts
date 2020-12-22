6d3485b3ee5da8c939ab6a11d800522c2cab9d4a
mohammad javad <mjkmo123@gmail.com>
2020-12-22 11:51:44 +0330

    add initial server to index

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import * as express from 'express';
 import {DB, port} from "./variables";
 import {connect} from "mongoose";
 import {registerMiddlewares} from "./utils/boot";
+import {initialSocket} from "./socket";
 
 const app = express();
 
 // middleware
 registerMiddlewares(app);
 
-app.listen(port || 3000, () => {
+const server = app.listen(port || 3000, () => {
     console.log('server is running')
-})
+});
+
+initialSocket(server);
 
 connect(DB, { useNewUrlParser: true, useUnifiedTopology: true },()=> {
     console.log('connected to DB')
