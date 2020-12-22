b149ba841b37dc0891920d2ba8ac0fd51390f134
mohammad javad <mjkmo123@gmail.com>
2020-12-22 08:27:26 +0330

    create get user middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import {verify} from "jsonwebtoken";
 import {secret} from "../variables";
+import {User} from "../schema/User";
 
 export async function expressAuthMiddleware(req: Request, res: Response, next: NextFunction) {
     // get jwt form header
@@ -20,4 +21,16 @@ export async function expressAuthMiddleware(req: Request, res: Response, next: N
     }catch (e) {
         res.send('invalid token');
     }
+}
+
+export async function getUser(req: Request, res: Response, next: NextFunction){
+
+    // @ts-ignore
+    const user = await User.findById(req.user._id);
+
+    // @ts-ignore
+    req.user = user;
+
+    next();
+
 }
