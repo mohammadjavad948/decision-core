c5c06e664e7cd4752c088d1c0a3ed24187c5caa4
mohammad javad <mjkmo123@gmail.com>
2020-12-22 11:32:39 +0330

    create login function for socket

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,3 +1,27 @@
-export function initialSocket(server){
+import {Socket} from "socket.io";
+import {verify} from "jsonwebtoken";
+import {secret} from "../variables";
 
+const socketIO = require('socket.io');
+
+export function initialSocket(server){
+    // initial server
+    const io = socketIO(server);
+
+    io.on('connecting', connection);
+}
+
+function connection(socket: Socket){
+
+    socket.on('login', async (token: string) => {
+
+    })
+}
+
+async function login(token: string){
+    try {
+        const userToken = verify(token, secret);
+    }catch (e){
+
+    }
 }
