80f78d2505d5b683abe1edddad7a468009d0199a
mohammad javad <mjkmo123@gmail.com>
2020-12-22 16:32:50 +0330

    create mixPermission function

--- src/mixer/roleMixer.ts
+++ src/mixer/roleMixer.ts
@@ -4,9 +4,15 @@ import {PermissionI} from "../schema/interface/permission";
 import {ChannelI, RoleI} from "../schema/interface/schemaInteface";
 
 export function mixer(userRoles: RoleI[], channelPermissions: ChannelI['permissions']): PermissionI{
+    // initial some var for holding data
     const roleID: string[] = [];
     let finalPermission: PermissionI;
 
+    for (let role of userRoles){
+
+    }
+
+    return finalPermission;
 }
 
 function intersection(array1: string[], array2: string[]): string[]{
@@ -15,4 +21,13 @@ function intersection(array1: string[], array2: string[]): string[]{
     return array1.filter(
         element => array2.includes(element)
     )
+}
+
+function mixPermission(permission1: PermissionI, permission2: PermissionI): PermissionI{
+    return {
+        sendMessage: permission1.sendMessage || permission2.sendMessage,
+        viewChannel: permission1.viewChannel || permission2.viewChannel,
+        manageQuiz: permission1.manageQuiz || permission2.manageQuiz,
+        manageHomeWorks: permission1.manageHomeWorks || permission2.manageHomeWorks,
+    }
 }
