2f49bd6a4478c4414116de230e03ac8059c191bd
mohammad javad <mjkmo123@gmail.com>
2020-12-23 08:46:48 +0330

    finished hasPermission function

--- src/mixer/permission.ts
+++ src/mixer/permission.ts
@@ -1,8 +1,31 @@
-import {ChannelI, UserI} from "../schema/interface/schemaInteface";
+import {ChannelI, RoleI, UserI} from "../schema/interface/schemaInteface";
+import {mixer} from "./roleMixer";
 
+// define permission type
+type permission = 'sendMessage' | 'viewChannel' | 'manageQuiz' | 'manageHomeWorks';
 
-export function hasPermission(user: UserI, channel: ChannelI, permissions: string | string[]): boolean{
+export function hasPermission(user: UserI, channel: ChannelI, permissions: permission | permission[]): boolean{
 
+    // check if user is admin return true for everything
+    if (user.admin) return true;
+
+    // mix permissions
+    const mainPermission = mixer(
+        user.roles as RoleI[],
+        channel.permissions
+    );
+
+    // if it need one permission
+    if (!Array.isArray(permissions)) return mainPermission[permissions];
+
+    // else
+    let varToReturn = true;
+
+    permissions.forEach(e => {
+        varToReturn = mainPermission[e] && varToReturn;
+    })
+
+    return varToReturn;
 }
 
 export function getPermissions(){
