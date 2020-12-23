ea8d3eb74288418d5d53befb816cba73fa9db32f
mohammad javad <mjkmo123@gmail.com>
2020-12-23 13:26:20 +0330

    basic online manager

--- src/socket/online.ts
+++ src/socket/online.ts
@@ -1,5 +1,14 @@
 import {Socket} from "socket.io";
+import {UserI} from "../schema/interface/schemaInteface";
+
+// create variables to hold online users
+let onlineUsers: string[] = [];
 
 export function manageOnlineUsers(io, socket: Socket){
+    // @ts-ignore
+    const user: UserI = socket.user;
 
+    onlineUsers.push(user._id);
+
+    socket.broadcast.emit('newOnline', user._id);
 }
