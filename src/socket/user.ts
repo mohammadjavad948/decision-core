7a77626d02e43ef2bd592b55904f8d713e1ef69d
mohammad javad <mjkmo123@gmail.com>
2020-12-24 08:02:49 +0330

    finish new user function

--- src/socket/user.ts
+++ src/socket/user.ts
@@ -1,5 +1,29 @@
 import {Socket} from "socket.io";
+import {genSalt, hash} from "bcrypt";
+import {User} from "../schema/User";
 
 export function UserManager(io, socket: Socket){
+    socket.on('createNewUser', newUser)
 
+    async function newUser(data, callback){
+
+        const name = data.name;
+        const username = data.username;
+        const rawPassword = data.password;
+
+        // hash password
+        const salt = await genSalt(10);
+        const password = await hash(rawPassword, salt);
+
+        // save user
+        const user = new User({
+            name, username, password, admin: true, roles: []
+        });
+
+        await user.save();
+
+        callback({
+            ok: true
+        })
+    }
 }
